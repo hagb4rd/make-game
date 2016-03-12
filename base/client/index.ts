df2281a0d2b942612db910c8c87ec4bf24f3c11f
Elisée Maurer <elisee@sparklinlabs.com>
2016-03-12 20:17:25 +0100

    client: Don't reconnect automatically, display message when disconnected

--- base/client/index.ts
+++ base/client/index.ts
@@ -2,4 +2,10 @@
 
 import * as io from "socket.io-client";
 
-const socket = io.connect(null, { transports: [ "websocket" ] });
+const socket = io.connect(null, { transports: [ "websocket" ], reconnection: false });
+
+socket.on("disconnect", onDisconnect);
+
+function onDisconnect() {
+  document.body.innerHTML = "Whoops, you have been disconnected. Plz reload the page.";
+}
